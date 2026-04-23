deb673fd09598306475db898af3d8641a16502a6
Brannon Lown <blown@charlotte.edu>
2026-04-22 20:36:17 -0400

    header login styled

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,1 +1,24 @@
-Login
+import React from 'react';
+
+function Login() {
+        const [userData, setUserData] = useState({
+            username: '',
+            password: '',
+        });
+    return (
+        <div className="LoginPage">
+            <input 
+            className="InputInfo" 
+            value={userData.username} 
+            onChange={(e) => setUserData({...userData, username: e.target.value})}
+            />
+            <input 
+            className="InputInfo" 
+            value={userData.password} 
+            onChange={(e) => setUserData({...userData, password: e.target.value})}
+            />
+        </div>
+    )
+}
+
+export default Login;
